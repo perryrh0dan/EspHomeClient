77eac649348a31d76ebe95e70deaa68b16980713
perryrh0dan <thomaspoehlmann96@googlemail.com>
2020-12-28 15:02:00 +0100

    new structure

--- examples/radar/radar.ino
+++ examples/radar/radar.ino
@@ -1,4 +1,19 @@
-#include "radar.h"
+#include <EspHomeClient.h>
+
+// WiFi & MQTT Server
+const char* ssid = "";
+const char* password = "";
+const char* mqtt_broker = "";
+const char* mqtt_username = "";
+const char* mqtt_password = "";
+const char* mqtt_client_name = "office_radar_1";
+
+// Sensor
+const int RADAR_SENSOR_PIN = 5;
+
+EspHomeClient client(
+  ssid, password, mqtt_broker, mqtt_username, mqtt_password, mqtt_client_name
+);
 
 void setup() {
   Serial.begin(115200);
